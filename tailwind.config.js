5d03436eb4efa708cd8b8aeff8d6cad71aa117ff
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-02-23 16:15:02 +0330

    chore: box shadow in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -483,6 +483,13 @@ export default {
     },
     fontFamily: { inter: 'Inter' },
     boxShadow: {
+      xs: '0px 1px 2px 0px rgba(16,24,40,0.05)',
+      sm: '0px 1px 2px 0px rgba(16,24,40,0.06), 0px 1px 3px 0px rgba(16,24,40,0.1)',
+      md: '0px 2px 4px -2px rgba(16,24,40,0.06), 0px 4px 8px -2px rgba(16,24,40,0.1)',
+      lg: '0px 4px 6px -2px rgba(16,24,40,0.03), 0px 12px 16px -4px rgba(16,24,40,0.08)',
+      xl: '0px 8px 8px -4px rgba(16,24,40,0.03), 0px 20px 24px -4px rgba(16,24,40,0.08)',
+      '2xl': '0px 24px 48px -12px rgba(16,24,40,0.18)',
+      '3xl': '0px 32px 64px -12px rgba(16,24,40,0.14)',
       'Focus rings/ring-brand': '0px 0px 0px 4px rgba(51,110,107,0.24)',
       'Focus rings/ring-brand-shadow-xs': '0px 0px 0px 4px rgba(51,110,107,0.24), 0px 1px 2px 0px rgba(16,24,40,0.05)',
       'Focus rings/ring-brand-shadow-sm':
