36487e20889a5b5b8114546b5b318ad58bf396cb
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-02-25 23:03:17 +0330

    feat: adaptors/apis/types for uploading media

---
+++ src/core/adaptors/media/index.adaptors.ts
@@ -0,0 +1,53 @@
+// import { uploadMedia, uploadMediaWithProgress } from 'src/core/api';
+
+import { AdaptorRes } from '..';
+import { UploadMediaRes } from './index.types';
+
+export const uploadMediaAdaptor = async (file: File): Promise<AdaptorRes<UploadMediaRes>> => {
+  try {
+    // const res = await uploadMedia(file);
+    return {
+      data: {
+        // ...res,
+        identity_id: '',
+        id: '1',
+        filename: 'test.png',
+        url: 'https://www.google.com',
+        created_at: '2002',
+        // created_at: res.created_at.toString(),
+      },
+      error: null,
+    };
+  } catch {
+    return {
+      data: null,
+      error: 'Error in uploadMedia API Call',
+    };
+  }
+};
+
+export const uploadMediaWithProgressAdaptor = async (
+  file: File,
+  setProgress: (val: number) => void,
+): Promise<AdaptorRes<UploadMediaRes>> => {
+  try {
+    // const res = await uploadMediaWithProgress(file, setProgress);
+    return {
+      data: {
+        // ...res,
+        identity_id: '',
+        id: '1',
+        filename: 'test.png',
+        url: 'https://www.google.com',
+        created_at: '2002',
+        // created_at: res.created_at.toString(),
+      },
+      error: null,
+    };
+  } catch {
+    return {
+      data: null,
+      error: 'Error in uploadMedia API Call',
+    };
+  }
+};
--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -1,4 +1,7 @@
 export * from './users/index.types';
 
+export * from './media/index.api';
+export * from './media/index.types';
+
 export * from './projects/index.api';
 export * from './projects/index.types';
