6bc7048c068935fdf7601049d7a7a6685d9f9121
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-03-03 22:29:14 +0330

    feat: adaptors/APIs/types for voting/donating projects

--- src/core/adaptors/projects/index.adaptors.ts
+++ src/core/adaptors/projects/index.adaptors.ts
@@ -1,51 +1,50 @@
-// import { getProjects, getProject } from 'src/core/api';
+// import { getProjects, getProject, vote, donate } from 'src/core/api';
 import { IdentityType } from 'src/core/api';
 
-import { AdaptorRes, Project, ProjectRes } from '..';
+import { AdaptorRes, DonateReq, Project, ProjectRes, SuccessRes } from '..';
 
 export const getProjectsAdaptor = async (page = 1, limit = 10): Promise<AdaptorRes<ProjectRes>> => {
-  //   const { items, total } = await getProjects({ page, limit });
-  //FIXME: mapping from API to adaptor
-  const projects = [
-    {
-      id: '1',
-      coverImg: '/images/explorer-cover.png',
-      category: 'Gender Equality',
-      title: 'Empowering Women Through Education',
-      description:
-        'This initiative focuses on providing access to quality education and vocational training for women in underserved communities, empowering them to break the cycle of poverty.',
-      creator: { type: 'users' as IdentityType, name: 'EduWomen Alliance', img: '' },
-    },
-    {
-      id: '2',
-      coverImg: '/images/explorer-cover.png',
-      category: 'Food Security',
-      title: 'Sustainable Farming Solutions',
-      description:
-        'This project supports small-scale farmers in adopting sustainable agriculture practices, improving food security, and reducing environmental degradation.',
-      creator: { type: 'users' as IdentityType, name: 'Alejandro Torres', img: '' },
-    },
-    {
-      id: '3',
-      coverImg: '/images/explorer-cover.png',
-      category: 'Health',
-      title: 'Accessible Healthcare for Remote Communities',
-      description:
-        'This initiative aims to establish mobile health clinics and telemedicine services in remote areas, providing access to quality healthcare for underserved populations.',
-      creator: { type: 'organizations' as IdentityType, name: 'HealthReach International', img: '' },
-    },
-    {
-      id: '4',
-      coverImg: '/images/explorer-cover.png',
-      category: 'Environment and Sustainability',
-      title: 'Clean Water for All',
-      description:
-        'This initiative aims to establish mobile health clinics and telemedicine services in remote areas, providing access to quality healthcare for underserved populations.',
-      creator: { type: 'users' as IdentityType, name: 'Maria Rodriguez', img: '' },
-    },
-  ];
-
   try {
+    //   const { items, total } = await getProjects({ page, limit });
+    //FIXME: mapping from API to adaptor
+    const projects = [
+      {
+        id: '1',
+        coverImg: '/images/explorer-cover.png',
+        category: 'Gender Equality',
+        title: 'Empowering Women Through Education',
+        description:
+          'This initiative focuses on providing access to quality education and vocational training for women in underserved communities, empowering them to break the cycle of poverty.',
+        creator: { type: 'users' as IdentityType, name: 'EduWomen Alliance', img: '' },
+      },
+      {
+        id: '2',
+        coverImg: '/images/explorer-cover.png',
+        category: 'Food Security',
+        title: 'Sustainable Farming Solutions',
+        description:
+          'This project supports small-scale farmers in adopting sustainable agriculture practices, improving food security, and reducing environmental degradation.',
+        creator: { type: 'users' as IdentityType, name: 'Alejandro Torres', img: '' },
+      },
+      {
+        id: '3',
+        coverImg: '/images/explorer-cover.png',
+        category: 'Health',
+        title: 'Accessible Healthcare for Remote Communities',
+        description:
+          'This initiative aims to establish mobile health clinics and telemedicine services in remote areas, providing access to quality healthcare for underserved populations.',
+        creator: { type: 'organizations' as IdentityType, name: 'HealthReach International', img: '' },
+      },
+      {
+        id: '4',
+        coverImg: '/images/explorer-cover.png',
+        category: 'Environment and Sustainability',
+        title: 'Clean Water for All',
+        description:
+          'This initiative aims to establish mobile health clinics and telemedicine services in remote areas, providing access to quality healthcare for underserved populations.',
+        creator: { type: 'users' as IdentityType, name: 'Maria Rodriguez', img: '' },
+      },
+    ];
     return {
       data: {
         items: projects,
@@ -62,28 +61,32 @@ export const getProjectsAdaptor = async (page = 1, limit = 10): Promise<AdaptorR
 };
 
 export const getProjectAdaptor = async (projectId: string): Promise<AdaptorRes<Project>> => {
-  //   const project = await getProject(id);
-  //FIXME: mapping from API to adaptor
-  const data = {
-    id: '1',
-    coverImg: '/images/explorer-cover.png',
-    category: 'Gender Equality',
-    title: 'Empowering Women Through Education',
-    description:
-      'This initiative focuses on providing access to quality education and vocational training for women in underserved communities, empowering them to break the cycle of poverty.',
-    creator: { type: 'users' as IdentityType, name: 'EduWomen Alliance', username: '@EduWomenAlliance', img: '' },
-    website: 'empoweringwomenethiopia.org',
-    location: 'Addis Ababa, Ethiopia',
-    overview:
-      '"Empowering Ethiopia Future" is a project dedicated to providing accessible, high-quality education to children in underprivileged communities across Ethiopia. By establishing community-based learning centers, training local educators, and providing essential educational resources, we aim to bridge the education gap and create opportunities for every child to reach their full potential.',
-    roundStats: { estimatedMatch: 1240.4, donatedAmount: 24.3, votes: 2 },
-    donations: [
-      { id: '1', donated_identity: { name: 'Chris Willis' }, donated_price: '100.00 ADA', date: new Date().toString() },
-      { id: '2', donated_identity: { name: 'Anonymous' }, donated_price: '200.00 ADA', date: new Date().toString() },
-    ],
-  };
-
   try {
+    //   const project = await getProject(id);
+    //FIXME: mapping from API to adaptor
+    const data = {
+      id: '1',
+      coverImg: '/images/explorer-cover.png',
+      category: 'Gender Equality',
+      title: 'Empowering Women Through Education',
+      description:
+        'This initiative focuses on providing access to quality education and vocational training for women in underserved communities, empowering them to break the cycle of poverty.',
+      creator: { type: 'users' as IdentityType, name: 'EduWomen Alliance', username: '@EduWomenAlliance', img: '' },
+      website: 'empoweringwomenethiopia.org',
+      location: 'Addis Ababa, Ethiopia',
+      overview:
+        '"Empowering Ethiopia Future" is a project dedicated to providing accessible, high-quality education to children in underprivileged communities across Ethiopia. By establishing community-based learning centers, training local educators, and providing essential educational resources, we aim to bridge the education gap and create opportunities for every child to reach their full potential.',
+      roundStats: { estimatedMatch: 1240.4, donatedAmount: 24.3, votes: 2 },
+      donations: [
+        {
+          id: '1',
+          donated_identity: { name: 'Chris Willis' },
+          donated_price: '100.00 ADA',
+          date: new Date().toString(),
+        },
+        { id: '2', donated_identity: { name: 'Anonymous' }, donated_price: '200.00 ADA', date: new Date().toString() },
+      ],
+    };
     return {
       data,
       error: null,
@@ -91,5 +94,22 @@ export const getProjectAdaptor = async (projectId: string): Promise<AdaptorRes<P
   } catch (error) {
     console.error('Error in getting project detail: ', error);
     return { data: null, error: 'Error in getting project detail' };
+  }
+};
+
+export const voteOrDonateProjectAdaptor = async (
+  projectId: string,
+  donatePayload?: DonateReq,
+): Promise<AdaptorRes<SuccessRes>> => {
+  try {
+    if (donatePayload) {
+      // await donate(projectId, payload)
+    } else {
+      // await vote(projectId)
+    }
+    return { data: { message: 'succeed' }, error: null };
+  } catch (error) {
+    console.error('Error in voting/donating project: ', error);
+    return { data: null, error: 'Error in voting/donating project' };
   }
 };
--- src/core/api/projects/index.api.ts
+++ src/core/api/projects/index.api.ts
@@ -1,4 +1,4 @@
-import { get } from '../http';
+import { get, post } from '../http';
 import { FilterReq, PaginateReq } from '../types';
 
 export async function getProjects(params: PaginateReq, filters?: FilterReq): Promise<any> {
@@ -7,4 +7,12 @@ export async function getProjects(params: PaginateReq, filters?: FilterReq): Pro
 
 export async function getProject(id: string): Promise<any> {
   return (await get<any>(`projects/${id}`)).data;
+}
+
+export async function vote(id: string): Promise<any> {
+  return (await post<any>(`projects/${id}/vote`, {})).data;
+}
+
+export async function donate(id: string, payload: any): Promise<any> {
+  return (await post<any>(`projects/${id}/donate`, payload)).data;
 }
