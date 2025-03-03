225660763eb70e73e247817cab15e355d1b2264f
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-03-03 22:36:57 +0330

    feat: vote/donate currencies constant

---
+++ src/constants/CURRENCIES.ts
@@ -0,0 +1,22 @@
+export const CURRENCIES = [
+  {
+    label: 'ADA',
+    value: 'ADA',
+  },
+  {
+    label: 'Djed',
+    value: 'DJED',
+  },
+  {
+    label: 'USDA',
+    value: 'USDA',
+  },
+  {
+    label: 'USDM',
+    value: 'USDM',
+  },
+  {
+    label: 'SOCIO',
+    value: 'SOCIO',
+  },
+];
