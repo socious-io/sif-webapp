081c70e50e5adf7183658bd0cfc2f92be6c32ea8
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-03-03 22:33:51 +0330

    feat: successful vote/donate modal

---
+++ src/modules/Projects/components/SuccessModal/index.types.ts
@@ -0,0 +1,20 @@
+import { ModalProps } from 'src/modules/General/components/Modal/index.types';
+
+export type VoteInfo = {
+  receivedAmount: string;
+};
+
+export type DonateInfo = {
+  donate: string;
+  currency: string;
+  donateConversion: string;
+  estimatedMatch: string;
+  totalContribution: string;
+};
+
+export interface SuccessModalProps extends ModalProps {
+  projectTitle: string;
+  voteInfo?: VoteInfo;
+  donateInfo?: DonateInfo;
+  onContinue: () => void;
+}
