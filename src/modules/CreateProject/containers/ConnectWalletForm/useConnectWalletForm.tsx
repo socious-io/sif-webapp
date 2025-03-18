27345aafe378907b9a4785feec54bd9c9bd38d39
Ehsan Mahmoudi <me@e-mahmoudi.me>
2025-03-18 16:57:26 +0330

    feat(wallet): cardano connect

--- src/modules/CreateProject/containers/ConnectWalletForm/useConnectWalletForm.tsx
+++ src/modules/CreateProject/containers/ConnectWalletForm/useConnectWalletForm.tsx
@@ -1,19 +1,19 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import Connect from 'src/modules/General/components/ConnectButton';
 import { setProjectData } from 'src/store/reducers/createProject.reducer';
 
 export const useConnectWalletForm = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  // const { Web3Connect, account } = dapp.useWeb3();
-  const fake_account = '0x123';
+  const { ConnectButton, address, connected } = Connect();
   const navigatePublish = () => navigate('/create/publish');
   const goBack = () => navigate('/create/step-3');
   useEffect(() => {
-    dispatch(setProjectData({ wallet_address: fake_account }));
+    dispatch(setProjectData({ wallet_address: address }));
   }, []);
-  const isEnabled = !!fake_account;
-  return { navigatePublish, goBack, isEnabled };
+  const isEnabled = !!connected;
+  return { navigatePublish, goBack, isEnabled, ConnectButton };
 };
