9befe2e81846a54964ca6386d1622d0d8537fff4
Seira Yun <discoverdeepseira@gmail.com>
2025-03-29 09:38:52 +0900

    TAS-3349 changed social media links in footer

--- src/modules/Layout/components/Footer/statics.ts
+++ src/modules/Layout/components/Footer/statics.ts
@@ -2,7 +2,7 @@ import FacebookIcon from 'src/assets/icons/socials/facebook.svg';
 import LinkedInIcon from 'src/assets/icons/socials/linkedin.svg';
 import XIcon from 'src/assets/icons/socials/x.svg';
 export const socialLinks = [
-  { alt: 'X (Twitter)', src: XIcon, link: 'https://twitter.com' },
-  { alt: 'Facebook', src: FacebookIcon, link: 'https://facebook.com' },
-  { alt: 'LinkedIn', src: LinkedInIcon, link: 'https://linkedin.com' },
+  { alt: 'X (Twitter)', src: XIcon, link: 'https://x.com/SociousDAO' },
+  { alt: 'Facebook', src: FacebookIcon, link: 'https://www.facebook.com/socious.io' },
+  { alt: 'LinkedIn', src: LinkedInIcon, link: 'https://www.linkedin.com/company/socious-io' },
 ];
