67b8c67a7db5cbe1f8a07cb4ba9cce05e437fcd2
Seira Yun <discoverdeepseira@gmail.com>
2025-03-29 09:28:59 +0900

    TAS-3337 Days Until Not Negative

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -82,13 +82,13 @@ export const Home = () => {
               </div>
               <div className="flex-1 flex flex-col gap-1 text-sm text-Gray-light-mode-600">
                 <span className="text-4xl md:text-5xl font-semibold text-Brand-700">
-                  {roundIsClosed ? 0 : getDaysUntil(round?.submission_end_at as string)}
+                  {roundIsClosed ? 0 : Math.max(0, getDaysUntil(round?.submission_end_at as string))}
                 </span>
                 {translate('home-days-left-to-submit')}
               </div>
               <div className="flex-1 flex flex-col gap-1 text-sm text-Gray-light-mode-600">
                 <span className="text-4xl md:text-5xl font-semibold text-Brand-700">
-                  {roundIsClosed ? 0 : getDaysUntil(round?.voting_end_at as string)}
+                  {roundIsClosed ? 0 : Math.max(0, getDaysUntil(round?.voting_end_at as string))}
                 </span>
                 {translate('home-days-left-to-vote')}
               </div>
