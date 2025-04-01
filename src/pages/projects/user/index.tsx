835afa7cab6034275116ec099012c468d319ad5c
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2025-04-01 16:39:11 +0330

    fix: remove FIXME and resolve them

--- src/pages/projects/user/index.tsx
+++ src/pages/projects/user/index.tsx
@@ -1,3 +1,4 @@
+import { translate } from 'src/core/helpers/utils';
 import Breadcrumbs from 'src/modules/General/components/Breadcrumbs';
 import Button from 'src/modules/General/components/Button';
 import ProjectsList from 'src/modules/Projects/containers/ProjectsList';
@@ -13,14 +14,15 @@ const breadcrumbs = [
     link: '#',
   },
 ];
+
 export const UsersProjects = () => {
   return (
     <div className="container pt-6 pb-16 px-4">
       <Breadcrumbs items={breadcrumbs} />
       <div className="flex flex-col items-start md:flex-row md:items-center justify-between gap-8 pt-5 pb-6">
-        <h1 className="text-[30px] font-semibold text-Brand-800">Your Projects</h1>
+        <h1 className="text-[30px] font-semibold text-Brand-800">{translate('your-projects.title')}</h1>
         <Button color="primary" variant="contained">
-          Create New Project
+          {translate('your-projects.create-new')}
         </Button>
       </div>
       <div className="pt-8 border-t border-t-Gray-light-mode-300 border-solid border-b-0 border-l-0 border-r-0">
