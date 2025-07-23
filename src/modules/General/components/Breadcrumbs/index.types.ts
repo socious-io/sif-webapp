040706b6037a65c1ea116d5a8c49d91fc79feb62
Iman rb <iman.rezasefat@gmail.com>
2025-07-23 08:39:09 +0330

    filter projects by round and category

--- src/modules/General/components/Breadcrumbs/index.types.ts
+++ src/modules/General/components/Breadcrumbs/index.types.ts
@@ -1,11 +1,19 @@
 import { BreadcrumbsProps as MUIBreadcrumbProps } from '@mui/material';
 
-export type BreadcrumbItem = {
-  label: string;
-  link?: string;
-  iconName?: string;
-};
+export type BreadcrumbItem =
+  | {
+      label: string;
+      link?: string;
+      iconName?: string;
+    }
+  | {
+      label: string;
+      options: { label: string; value: string }[];
+      onChange?: (value) => void;
+      defaultValue: { label: string; value: string };
+    };
 
 export interface BreadcrumbsProps extends MUIBreadcrumbProps {
   items: BreadcrumbItem[];
+  className?: string;
 }
--- src/modules/Projects/containers/ProjectsList/useProjectsList.tsx
+++ src/modules/Projects/containers/ProjectsList/useProjectsList.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLoaderData, useNavigate } from 'react-router-dom';
+import { ProjectCategory } from 'src/constants/PROJECT_CATEGORIES';
 import { getProjectsAdaptor, ProjectRes } from 'src/core/adaptors';
 
-export const useProjectsList = () => {
+export const useProjectsList = (roundId: string) => {
   const navigate = useNavigate();
   const { projects } = useLoaderData() as { projects: ProjectRes };
   const [currentProjects, setCurrentProjects] = useState(projects);
@@ -11,12 +12,19 @@ export const useProjectsList = () => {
   const total = currentProjects?.total || 0;
   const currentList = currentProjects?.items || [];
   const totalPage = Math.ceil(total / limit);
-
+  const [projectCategory, setProjectCategory] = useState<ProjectCategory>({ id: '1', label: 'All', value: '' });
   const onChangePage = async (newPage: number) => {
     setPage(newPage);
     const { data } = await getProjectsAdaptor(newPage, limit);
     data && setCurrentProjects(data);
   };
+  useEffect(() => {
+    const changeRound = async () => {
+      const { data } = await getProjectsAdaptor(1, limit, { round_id: roundId, category: projectCategory.value });
+      data && setCurrentProjects(data);
+    };
+    changeRound();
+  }, [roundId, projectCategory]);
 
   return {
     data: {
@@ -24,10 +32,12 @@ export const useProjectsList = () => {
       total,
       page,
       totalPage,
+      projectCategory,
     },
     operations: {
       navigate,
       onChangePage,
+      setProjectCategory,
     },
   };
 };
